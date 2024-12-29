9d0e8652de21ac8c150953df57f193d483e02bdb
TaherML1 <aboayham250@gmail.com>
2024-12-29 13:05:44 +0300

    Update main.c

--- systemProgrammingProject/main.c
+++ systemProgrammingProject/main.c
@@ -53,84 +53,84 @@ void handle_command(char *line) {
         print_usage();
     } else if (strcmp(argv[0], "tlist") == 0) {
         if (argc != 2) {
-            printf(RED "Error: Missing directory parameter for 'slist' command.\n" RESET);
+            printf(RED "Error: Missing directory parameter for 'tlist' command.\n" RESET);
             print_usage();
             return;
         }
         list_directory(argv[1]);
     } else if (strcmp(argv[0], "tcopy") == 0) {
         if (argc != 3) {
-            printf(RED "Error: Missing source or destination parameter for 'scopy' command.\n" RESET);
+            printf(RED "Error: Missing source or destination parameter for 'tcopy' command.\n" RESET);
             print_usage();
             return;
         }
         copy_file(argv[1], argv[2]);
     } else if (strcmp(argv[0], "tremove") == 0) {
         if (argc != 2) {
-            printf(RED "Error: Missing file/directory parameter for 'sremove' command.\n" RESET);
+            printf(RED "Error: Missing file/directory parameter for 'tremove' command.\n" RESET);
             print_usage();
             return;
         }
         remove_file_or_directory(argv[1]);
     } else if (strcmp(argv[0], "tcreate") == 0) {
         if (argc != 2) {
-            printf(RED "Error: Missing directory parameter for 'screate' command.\n" RESET);
+            printf(RED "Error: Missing directory parameter for 'tcreate' command.\n" RESET);
             print_usage();
             return;
         }
         create_directory(argv[1]);
     } else if (strcmp(argv[0], "tcreatefile") == 0) {
         if (argc != 2) {
-            printf(RED "Error: Missing filename parameter for 'screatefile' command.\n" RESET);
+            printf(RED "Error: Missing filename parameter for 'tcreatefile' command.\n" RESET);
             print_usage();
             return;
         }
         create_file(argv[1]);
     } else if (strcmp(argv[0], "tchmod") == 0) {
         if (argc != 3) {
-            printf(RED "Error: Missing file/directory or permissions parameter for 'schmod' command.\n" RESET);
+            printf(RED "Error: Missing file/directory or permissions parameter for 'tchmod' command.\n" RESET);
             print_usage();
             return;
         }
         change_permissions(argv[1], strtol(argv[2], NULL, 8));
     } else if (strcmp(argv[0], "twrite") == 0) {
         if (argc != 3) {
-            printf(RED "Error: Missing filename or data parameter for 'swrite' command.\n" RESET);
+            printf(RED "Error: Missing filename or data parameter for 'twrite' command.\n" RESET);
             print_usage();
             return;
         }
         write_to_file(argv[1], argv[2]);
     } else if (strcmp(argv[0], "tread") == 0) {
         if (argc != 2) {
-            printf(RED "Error: Missing filename parameter for 'sread' command.\n" RESET);
+            printf(RED "Error: Missing filename parameter for 'tread' command.\n" RESET);
             print_usage();
             return;
         }
         read_from_file(argv[1]);
     } else if (strcmp(argv[0], "topen") == 0) {
         if (argc != 2) {
-            printf(RED "Error: Missing directory parameter for 'sopen' command.\n" RESET);
+            printf(RED "Error: Missing directory parameter for 'topen' command.\n" RESET);
             print_usage();
             return;
         }
         open_folder(argv[1]);
     } else if (strcmp(argv[0], "tinfo") == 0) {
         if (argc != 2) {
-            printf(RED "Error: Missing filename parameter for 'sinfo' command.\n" RESET);
+            printf(RED "Error: Missing filename parameter for 'tinfo' command.\n" RESET);
             print_usage();
             return;
         }
         get_file_info(argv[1]);
     } else if (strcmp(argv[0], "tedit") == 0) {
         if (argc != 2) {
-            printf(RED "Error: Missing filename parameter for 'sedit' command.\n" RESET);
+            printf(RED "Error: Missing filename parameter for 'tedit' command.\n" RESET);
             print_usage();
             return;
         }
         edit_file(argv[1]);
     } else if (strcmp(argv[0], "trename") == 0) {
         if (argc != 3) {
-            printf(RED "Error: Missing oldname or newname parameter for 'srename' command.\n" RESET);
+            printf(RED "Error: Missing oldname or newname parameter for 'trename' command.\n" RESET);
             print_usage();
             return;
         }
